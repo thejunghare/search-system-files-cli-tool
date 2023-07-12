39de0dec81847e0450740ded44a95e80e9e6d61f
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-12 11:15:35 +0530

    updated main.go and root.go

--- cmd/root.go
+++ cmd/root.go
@@ -9,3 +9,7 @@ var rootCmd = &cobra.Command{
 	Use:   "Search files",
 	Short: "A CLI tool is used to search files in system.",
 }
+
+func init() {
+	rootCmd.AddCommand(rootCmd)
+}
--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/thejunghare/searchfiles/cmd"
+	"github.com/thejunghare/cmd"
 )
 
 func main() {
-	checkError(cmd.rootCmd.Execute())
-}
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Println("Something went wrong", err)
-	}
+	cmd.rootCmd.Execute()
 }
